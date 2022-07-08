ca9b99390c972057a07d4116cc59fae9e17c73c2
John Brunton <john_brunton@live.co.uk>
2022-07-08 15:52:54 +0100

    wip

--- ch03/main.hs
+++ ch03/main.hs
@@ -1,5 +1,21 @@
 import Data.List
 
+-- Exercises 1.
+
+-- Question 1.
+data List a = Cons a (List a)
+            | Nil
+              deriving (Show)
+
+-- Question 2.
+toList Nil = []
+toList (Cons x xs) = x:(toList xs)
+
+fromList [] = Nil
+fromList (x:xs) = Cons x (fromList xs)
+
+-- Exercises 2.
+
 -- Question 1.
 mylen [] = 0
 mylen (x:xs) = 1 + mylen xs
@@ -50,18 +66,22 @@ height (Node x t1 t2) = 1 + max (height t1) (height t2)
 
 -- Question 9.
 data Direction = Left | Right | Straight
+  deriving Show
 
 -- Question 10.
--- data Point = Point { ptX :: Double, ptY :: Double }
--- direction :: (Point, Point, Point) -> Direction
--- direction ((ax, ay), (bx, by), (cx, cy)) = dir alpha
---   where alpha = acos (dotp / (sizeV1 * sizeV2))
---         dir 0 = Main.Straight
---         dir a | a < 0 = Main.Left
---         dir a | a > 0 = Main.Right
---         where dotp = (ptX v1 * ptX v2 + ptY v1 * ptY v2)
---               sizeV1 = sqrt ((ptX v1)**2 + (ptY v1)**2)
---               sizeV2 = sqrt ((ptX v2)**2 + (ptY v2)**2)
---               where v1 = (bx - ax, by - ay)
---                     v2 = (cx - bx, cy - by)
-        
+type Vec2 = (Double, Double)
+
+dotp :: Vec2 -> Vec2 -> Double
+dotp (x1, y1) (x2, y2) = x1 * x2 + y1 * y2
+
+size :: Vec2 -> Double
+size (x, y) = sqrt (x**2 + y**2)
+
+direction :: Vec2 -> Vec2 -> Vec2 -> Direction
+direction (ax, ay) (bx, by) (cx, cy) = dir alpha
+  where alpha = atan2 (snd v2) (fst v2) - atan2 (snd v1) (fst v1)
+        dir 0 = Main.Straight
+        dir a | a < 0 = Main.Left
+        dir a | a > 0 = Main.Right
+        v1 = (bx - ax, by - ay)
+        v2 = (cx - bx, cy - by)
