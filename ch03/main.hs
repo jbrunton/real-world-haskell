4e912eaab7e41369fff2cccc85585951336f366c
John Brunton <john_brunton@live.co.uk>
2022-07-08 12:59:47 +0100

    wip

--- ch03/main.hs
+++ ch03/main.hs
@@ -38,3 +38,30 @@ sortLists xs = sortBy (\x y -> compare (length x) (length y)) xs
 
 -- Question 7.
 -- See intersperse.hs
+
+-- Question 8.
+data Tree a = Node a (Tree a) (Tree a)
+            | Empty
+              deriving (Show)
+
+height :: Tree a -> Int
+height Empty = 0
+height (Node x t1 t2) = 1 + max (height t1) (height t2)
+
+-- Question 9.
+data Direction = Left | Right | Straight
+
+-- Question 10.
+-- data Point = Point { ptX :: Double, ptY :: Double }
+-- direction :: (Point, Point, Point) -> Direction
+-- direction ((ax, ay), (bx, by), (cx, cy)) = dir alpha
+--   where alpha = acos (dotp / (sizeV1 * sizeV2))
+--         dir 0 = Main.Straight
+--         dir a | a < 0 = Main.Left
+--         dir a | a > 0 = Main.Right
+--         where dotp = (ptX v1 * ptX v2 + ptY v1 * ptY v2)
+--               sizeV1 = sqrt ((ptX v1)**2 + (ptY v1)**2)
+--               sizeV2 = sqrt ((ptX v2)**2 + (ptY v2)**2)
+--               where v1 = (bx - ax, by - ay)
+--                     v2 = (cx - bx, cy - by)
+        
